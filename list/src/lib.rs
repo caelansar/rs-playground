9cc7149b94de9d9aae910c794a7625e21cdbe721
caelansar <819711623@qq.com>
2022-10-30 20:09:22 +0800

    feat: play list

---
+++ list/src/lib.rs
@@ -0,0 +1,204 @@
+pub struct List<T> {
+    head: Link<T>,
+}
+
+type Link<T> = Option<Box<Node<T>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+}
+
+impl<T> List<T> {
+    /// Constructs a new, empty `List<T>`.
+    pub fn new() -> Self {
+        List { head: None }
+    }
+
+    /// Insert an element to the front of a `List<T>`.
+    pub fn push(&mut self, elem: T) {
+        let head = Some(Box::new(Node {
+            elem,
+            next: self.head.take(),
+        }));
+        self.head = head
+    }
+
+    /// Removes the first element from a `List<T>` and returns it, or [`None`] if it
+    /// is empty.
+    pub fn pop(&mut self) -> Option<T> {
+        let node = self.head.take().map(|n| {
+            self.head = n.next;
+            n.elem
+        });
+        node
+    }
+
+    /// Returns a reference to the element in the head of `List<T>`
+    pub fn peek(&self) -> Option<&T> {
+        let node = self.head.as_ref().map(|x| &x.elem);
+        node
+    }
+
+    /// Returns a mutable reference to the element in the head of the `List<T>`
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        let node = self.head.as_deref_mut().map(|x| &mut x.elem);
+        node
+    }
+
+    /// Creates a consuming iterator, that is, one that moves each value out of
+    /// the list. The list cannot be used after calling this.
+    pub fn into_iter(self) -> IntoIter<T> {
+        IntoIter(self)
+    }
+
+    /// Returns an iterator over the list.
+    pub fn iter(&self) -> Iter<'_, T> {
+        Iter {
+            next: self.head.as_deref(),
+        }
+    }
+
+    /// Returns an iterator that allows modifying each value.
+    pub fn iter_mut(&mut self) -> IterMut<'_, T> {
+        IterMut {
+            next: self.head.as_deref_mut(),
+        }
+    }
+}
+
+impl<T> Drop for List<T> {
+    fn drop(&mut self) {
+        let mut cur_link = self.head.take();
+        while let Some(mut node) = cur_link {
+            cur_link = node.next.take();
+        }
+    }
+}
+
+pub struct IntoIter<T>(List<T>);
+
+impl<T> Iterator for IntoIter<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
+
+pub struct Iter<'a, T> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+    type Item = &'a T;
+    fn next(&mut self) -> Option<Self::Item> {
+        // self.next.map it is fine because &Node<T> is `Copy`
+        // and Option<&Node<T>> is also `Copy`
+        self.next.map(|x| {
+            self.next = x.next.as_deref();
+            &x.elem
+        })
+    }
+}
+
+pub struct IterMut<'a, T> {
+    next: Option<&'a mut Node<T>>,
+}
+
+impl<'a, T> Iterator for IterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        // &mut isn't `Copy`, instead, we should `Take` the ownership to get it.
+        self.next.take().map(|x| {
+            self.next = x.next.as_deref_mut();
+            &mut x.elem
+        })
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::List;
+
+    #[test]
+    fn basics_should_work() {
+        let mut list = List::new();
+
+        assert_eq!(list.pop(), None);
+
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.pop(), Some(3));
+        assert_eq!(list.pop(), Some(2));
+
+        list.push(4);
+        list.push(5);
+
+        assert_eq!(list.pop(), Some(5));
+        assert_eq!(list.pop(), Some(4));
+
+        assert_eq!(list.pop(), Some(1));
+        assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn peek_should_work() {
+        let mut list = List::new();
+        assert_eq!(list.peek(), None);
+        assert_eq!(list.peek_mut(), None);
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        assert_eq!(list.peek(), Some(&3));
+        assert_eq!(list.peek_mut(), Some(&mut 3));
+
+        list.peek_mut().map(|value| *value = 42);
+
+        assert_eq!(list.peek(), Some(&42));
+        assert_eq!(list.pop(), Some(42));
+    }
+
+    #[test]
+    fn into_iter_should_work() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.into_iter();
+        assert_eq!(iter.next(), Some(3));
+        assert_eq!(iter.next(), Some(2));
+        assert_eq!(iter.next(), Some(1));
+        assert_eq!(iter.next(), None);
+    }
+
+    #[test]
+    fn iter_should_work() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.iter();
+        assert_eq!(iter.next(), Some(&3));
+        assert_eq!(iter.next(), Some(&2));
+        assert_eq!(iter.next(), Some(&1));
+    }
+
+    #[test]
+    fn iter_mut_should_work() {
+        let mut list = List::new();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+
+        let mut iter = list.iter_mut();
+        assert_eq!(iter.next(), Some(&mut 3));
+        assert_eq!(iter.next(), Some(&mut 2));
+        assert_eq!(iter.next(), Some(&mut 1));
+    }
+}
