c0da4187c2944e2cb638fc11b58cc32cee8847be
caelansar <819711623@qq.com>
2023-02-17 22:50:18 +0800

    feat(thread): arc with mutex

--- thread/src/lib.rs
+++ thread/src/lib.rs
@@ -18,11 +18,30 @@ fn thread_scope() {
 
 #[cfg(test)]
 mod tests {
+    use std::{
+        sync::{Arc, Mutex},
+        time::Duration,
+    };
+
     use super::*;
 
     #[test]
     fn thread_should_works() {
         thread_spawn();
         thread_scope();
+    }
+
+    #[test]
+    fn thread_concurrency() {
+        let data = Arc::new(Mutex::new(1));
+        let data1 = data.clone();
+
+        let handler = thread::spawn(move || {
+            thread::sleep(Duration::from_secs(1));
+            assert_eq!(100, *data.lock().unwrap())
+        });
+
+        *data1.lock().unwrap() = 100;
+        handler.join().unwrap();
     }
 }
