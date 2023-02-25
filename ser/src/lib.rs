e256e392ab496f70e83bae3f3155ef283fdd29e4
caelansar <819711623@qq.com>
2023-02-25 12:41:00 +0800

    feat(serde): play zero-copy deserialization

--- ser/src/lib.rs
+++ ser/src/lib.rs
@@ -1,3 +1,6 @@
+#![feature(test)]
+extern crate test;
+
 use std::{fmt::Debug, marker::PhantomData};
 
 use serde::{Deserialize, Serialize};
@@ -67,7 +70,23 @@ fn parse<'a, P: ConfigParser<'a>>(data: &'a str, parser: P) -> Result<P::Cfg, Pa
 
 #[cfg(test)]
 mod tests {
+    use std::borrow::Cow;
+
+    use serde_json::json;
+    use test::Bencher;
+
     use super::*;
+
+    #[derive(Debug, Deserialize)]
+    struct CowUser<'a> {
+        #[serde(borrow)]
+        name: Cow<'a, str>,
+    }
+
+    #[derive(Debug, Deserialize)]
+    struct OwnedUser {
+        name: String,
+    }
 
     #[test]
     fn parse_yaml_should_works() {
@@ -83,9 +102,35 @@ mod tests {
     fn parse_json_should_works() {
         let point = Point { x: 1.0, y: 2.0 };
 
-        let yaml = r#"{"x": 1.0, "y": 2.0}"#;
+        let json = r#"{"x": 1.0, "y": 2.0}"#;
         let parser: JsonParser<Point> = JsonParser::new();
-        let cfg = parse(yaml, parser).unwrap();
+        let cfg = parse(json, parser).unwrap();
         assert_eq!(point, cfg);
+    }
+
+    #[bench]
+    fn bench_owned_user(b: &mut Bencher) {
+        let data = [0u8; 102400];
+        let s = String::from_utf8_lossy(&data);
+        let v = json!({"name": s, "age": 20});
+        let json = serde_json::to_string_pretty(&v).unwrap();
+        b.iter(|| {
+            let parser: JsonParser<OwnedUser> = JsonParser::new();
+            let user = parse(&json, parser).unwrap();
+            dbg!(user.name);
+        })
+    }
+
+    #[bench]
+    fn bench_cow_user(b: &mut Bencher) {
+        let data = [0u8; 102400];
+        let s = String::from_utf8_lossy(&data);
+        let v = json!({"name": s, "age": 20});
+        let json = serde_json::to_string_pretty(&v).unwrap();
+        b.iter(|| {
+            let parser: JsonParser<CowUser> = JsonParser::new();
+            let user = parse(&json, parser).unwrap();
+            dbg!(user.name);
+        })
     }
 }
