0fcd24f95340134821776aa333542720f18c54e9
caelansar <819711623@qq.com>
2023-03-12 11:42:47 +0800

    feat(hrtb): add closure example

--- hrtb/src/lib.rs
+++ hrtb/src/lib.rs
@@ -43,8 +43,27 @@ impl<'a, T: ?Sized> Trait<&'a T> for T {
 //} // - `x` dropped here while still borrowed
 
 fn bar(b: impl for<'a> Trait<&'a usize>) {
-    let x: usize = 10;
-    b.do_something(&x);
+    #[allow(unused_labels)]
+    'y: {
+        let x: usize = 10;
+        b.do_something(&x);
+    }
+}
+
+struct Closure<F> {
+    data: (u8, u16),
+    func: F,
+}
+impl<F> Closure<F>
+where
+    for<'a> F: Fn(&'a (u8, u16)) -> &'a u8,
+{
+    fn call(&self) -> &u8 {
+        (self.func)(&self.data)
+    }
+}
+fn do_it(data: &(u8, u16)) -> &u8 {
+    &data.0
 }
 
 #[cfg(test)]
@@ -52,11 +71,26 @@ mod tests {
     use super::*;
 
     #[test]
+    fn closure_should_work() {
+        #[allow(unused_labels)]
+        'x: {
+            let clo = Closure {
+                data: (0, 1),
+                func: do_it,
+            };
+            println!("{}", clo.call());
+        }
+    }
+
+    #[test]
     fn do_something_should_works() {
         let s = "aa";
         (&s).do_something("bb");
 
-        bar(1usize);
+        #[allow(unused_labels)]
+        'x: {
+            bar(1usize);
+        }
     }
 
     #[test]
