53fd8985f5cb03498a471a25bc75bd1ae2ff0711
caelansar <819711623@qq.com>
2023-03-15 21:53:16 +0800

    chore: lifetime annotation problem

--- gat/src/lending_iter.rs
+++ gat/src/lending_iter.rs
@@ -25,7 +25,7 @@ impl<'t, T> WindowsMut<'t, T> {
 impl<'t, T> LendingIterator for WindowsMut<'t, T> {
     type Item<'a> = &'a mut [T] where Self: 'a;
 
-    fn next<'a>(&'a mut self) -> Option<Self::Item<'a>> {
+    fn next(&mut self) -> Option<Self::Item<'_>> {
         let retval = self.slice[self.start..].get_mut(..self.window_size)?;
         self.start += self.window_size;
         Some(retval)
@@ -60,5 +60,46 @@ impl<I: Iterator> Iterator for Windows<I> {
         } else {
             Some(data)
         }
+    }
+}
+
+struct ByteIter<'a> {
+    data: &'a [u8],
+}
+
+// impl<'a> ByteIter<'a> {
+//     fn next<'b>(&'b mut self) -> Option<&'b u8> {
+//         if self.data.is_empty() {
+//             None
+//         } else {
+//             let byte = &self.data[0];
+//             self.data = &self.data[1..];
+//             Some(byte)
+//         }
+//     }
+// }
+
+impl<'a> ByteIter<'a> {
+    fn next<'b>(&'b mut self) -> Option<&'a u8> {
+        if self.data.is_empty() {
+            None
+        } else {
+            let byte = &self.data[0];
+            self.data = &self.data[1..];
+            Some(byte)
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::ByteIter;
+
+    #[test]
+    fn byte_iter_should_work() {
+        let mut byte_iter = ByteIter { data: b"112233" };
+        let b1 = byte_iter.next();
+        let b2 = byte_iter.next();
+        assert!(b1 == b2);
     }
 }
