13a5d21bba0462d47cc7f1c7506966bebcf5ed79
caelansar <819711623@qq.com>
2023-03-21 22:27:22 +0800

    feat(hrtb): implement Filter for Vec<T>

--- hrtb/src/lib.rs
+++ hrtb/src/lib.rs
@@ -1,3 +1,5 @@
+use std::fmt::Debug;
+
 trait OptionFilter<T> {
     fn filter1<F>(self, f: F) -> Option<T>
     where
@@ -8,6 +10,47 @@ impl<T> OptionFilter<T> for Option<T> {
     fn filter1<F>(self, f: F) -> Option<T>
     where
         F: for<'a> FnOnce(&'a T) -> bool,
+    {
+        if let Some(x) = self {
+            if f(&x) {
+                return Some(x);
+            }
+        }
+        None
+    }
+}
+
+trait Filter {
+    type Item;
+
+    type Output<U>;
+
+    fn filter<F>(self, f: F) -> Self::Output<Self::Item>
+    where
+        F: for<'a> Fn(&'a Self::Item) -> bool;
+}
+
+impl<T> Filter for Vec<T> {
+    type Item = T;
+
+    type Output<U> = Vec<U>;
+
+    fn filter<F>(self, f: F) -> Self::Output<Self::Item>
+    where
+        F: for<'a> Fn(&'a Self::Item) -> bool,
+    {
+        self.into_iter().filter(|x| f(x)).collect()
+    }
+}
+
+impl<T: Debug> Filter for Option<T> {
+    type Item = T;
+
+    type Output<U> = Option<U>;
+
+    fn filter<F>(self, f: F) -> Self::Output<Self::Item>
+    where
+        F: for<'a> Fn(&'a Self::Item) -> bool,
     {
         if let Some(x) = self {
             if f(&x) {
@@ -74,6 +117,13 @@ mod tests {
     use super::*;
 
     #[test]
+    fn vec_filter_should_work() {
+        let v = vec![1, 2, 3, 4];
+        let r = v.filter(|x| x % 2 == 0);
+        assert_eq!(vec![2, 4], r);
+    }
+
+    #[test]
     fn closure_should_work() {
         #[allow(unused_labels)]
         'x: {
@@ -99,7 +149,7 @@ mod tests {
     #[test]
     fn option_filter_works() {
         let v = Some(1);
-        assert_eq!(Some(1), v.filter1(|x| *x > 0));
-        assert_eq!(None, v.filter1(|x| *x < 0));
+        assert_eq!(Some(1), <Option<i32> as Filter>::filter(v, |x| *x > 0));
+        assert_eq!(None, <Option<i32> as Filter>::filter(v, |x| *x < 0));
     }
 }
