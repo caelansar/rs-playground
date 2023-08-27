185f394fc1e84def933febf0d6df4bc971ef19b5
caelansar <819711623@qq.com>
2023-08-27 16:49:27 +0800

    feat(function): manually build closure struct

--- function/src/lib.rs
+++ function/src/lib.rs
@@ -1,6 +1,9 @@
+#![feature(fn_traits)]
+#![feature(unboxed_closures)]
+
 #[cfg(test)]
 mod tests {
-    use std::mem::size_of_val;
+    use std::mem::{size_of_val, transmute};
 
     use std::any::type_name;
 
@@ -27,11 +30,73 @@ mod tests {
 
     #[test]
     fn test_function_pointer() {
-        let mut x: fn(i32) -> i32 = foo;
+        let mut x: fn(i32) -> i32 = foo; // coercion
         println!("{}", type_name_of_val(&x));
         assert_eq!(8, size_of_val(&x));
         x = bar;
         println!("{}", type_name_of_val(&x));
         assert_eq!(8, size_of_val(&x));
+    }
+
+    #[test]
+    fn test_cast() {
+        let ptr = foo as fn(i32) -> i32;
+        let ptr_addr = ptr as usize;
+
+        println!("{}", ptr_addr);
+
+        let raw_ptr = ptr_addr as *const ();
+        let ptr = unsafe { transmute::<_, fn(i32) -> i32>(raw_ptr) };
+        assert_eq!(0, ptr(1));
+    }
+
+    #[test]
+    fn test_manually_closure_struct() {
+        let name = "cae".to_string();
+        let vec = vec![1, 2, 3];
+        let v = &vec;
+        let data = (1, 2, 3);
+        let closure = move |x: i32| {
+            println!("param: {x}");
+            println!("data: {:?}", data);
+            println!("v: {:?}, name: {:?}", v, name.clone());
+        };
+        closure(1);
+        println!("vec: {:?}", vec); // still valid
+
+        struct Closure<Captured> {
+            captured: Captured,
+        }
+
+        impl<'a> FnOnce<(i32,)> for Closure<((i32, i32, i32), &'a Vec<i32>, String)> {
+            type Output = ();
+            extern "rust-call" fn call_once(self, args: (i32,)) {
+                self.call(args)
+            }
+        }
+
+        impl<'a> FnMut<(i32,)> for Closure<((i32, i32, i32), &'a Vec<i32>, String)> {
+            extern "rust-call" fn call_mut(&mut self, args: (i32,)) {
+                self.call(args)
+            }
+        }
+
+        impl<'a> Fn<(i32,)> for Closure<((i32, i32, i32), &'a Vec<i32>, String)> {
+            extern "rust-call" fn call(&self, (x,): (i32,)) {
+                println!("param: {x}");
+                println!("data: {:?}", self.captured.0);
+                println!("v: {:?}, name: {:?}", self.captured.1, self.captured.2);
+            }
+        }
+
+        let name = "cae".to_string();
+        let vec = vec![1, 2, 3];
+        let v = &vec;
+        let data = (1, 2, 3);
+        let manually_closure = Closure {
+            captured: (data, v, name),
+        };
+        manually_closure(1);
+        println!("vec: {:?}", vec); // still valid
     }
 }
