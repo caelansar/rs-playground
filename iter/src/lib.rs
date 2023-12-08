c0d4b164c62ace4ae3f163fef12d09361b92d606
caelansar <819711623@qq.com>
2023-12-08 21:48:53 +0800

    feat(iter): add map/filter adapter

--- iter/src/lib.rs
+++ iter/src/lib.rs
@@ -1,30 +1,46 @@
 use std::collections::HashMap;
 use std::hash::Hash;
 
-pub trait MyFromIterator<T> {
+pub trait FromIterator<T> {
     fn my_from_iter<I>(iter: I) -> Self
     where
-        I: MyIterator<Item = T>;
+        I: Iterator<Item = T>;
 }
 
-pub trait MyIterator {
+pub trait Iterator {
     type Item;
 
     fn next(&mut self) -> Option<Self::Item>;
 
     fn collect<B>(self) -> B
     where
-        B: MyFromIterator<Self::Item>,
+        B: FromIterator<Self::Item>,
         Self: Sized,
     {
         B::my_from_iter(self)
     }
+
+    fn map<B, F>(self, f: F) -> Map<Self, F>
+    where
+        Self: Sized,
+        F: FnMut(Self::Item) -> B,
+    {
+        Map::new(self, f)
+    }
+
+    fn filter<P>(self, predicate: P) -> Filter<Self, P>
+    where
+        Self: Sized,
+        P: FnMut(&Self::Item) -> bool,
+    {
+        Filter::new(self, predicate)
+    }
 }
 
-impl<T> MyFromIterator<T> for Vec<T> {
+impl<T> FromIterator<T> for Vec<T> {
     fn my_from_iter<I>(mut iter: I) -> Self
     where
-        I: MyIterator<Item = T>,
+        I: Iterator<Item = T>,
     {
         let mut vec = Vec::new();
 
@@ -36,16 +52,71 @@ impl<T> MyFromIterator<T> for Vec<T> {
     }
 }
 
-impl<K, V> MyFromIterator<(K, V)> for HashMap<K, V>
+impl<K, V> FromIterator<(K, V)> for HashMap<K, V>
 where
     K: Eq + Hash,
 {
-    fn my_from_iter<I: MyIterator<Item = (K, V)>>(mut iter: I) -> HashMap<K, V> {
+    fn my_from_iter<I: Iterator<Item = (K, V)>>(mut iter: I) -> HashMap<K, V> {
         let mut map = HashMap::new();
         while let Some((k, v)) = iter.next() {
             map.insert(k, v);
         }
         map
+    }
+}
+
+pub struct Map<I, F> {
+    iter: I,
+    f: F,
+}
+
+impl<I, F> Map<I, F> {
+    fn new(iter: I, f: F) -> Self {
+        Self { iter, f }
+    }
+}
+
+impl<B, I, F> Iterator for Map<I, F>
+where
+    I: Iterator,
+    F: FnMut(I::Item) -> B,
+{
+    type Item = B;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(x) = self.iter.next() {
+            Some((self.f)(x))
+        } else {
+            None
+        }
+    }
+}
+
+pub struct Filter<I, P> {
+    iter: I,
+    p: P,
+}
+
+impl<I, P> crate::Filter<I, P> {
+    fn new(iter: I, p: P) -> Self {
+        Self { iter, p }
+    }
+}
+
+impl<I, P> Iterator for crate::Filter<I, P>
+where
+    I: Iterator,
+    P: FnMut(&I::Item) -> bool,
+{
+    type Item = I::Item;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while let Some(x) = self.iter.next() {
+            if (self.p)(&x) {
+                return Some(x);
+            }
+        }
+        None
     }
 }
 
@@ -60,7 +131,7 @@ impl<'a, T> SliceIterator<'a, T> {
     }
 }
 
-impl<T: Clone> MyIterator for SliceIterator<'_, T> {
+impl<T: Clone> Iterator for SliceIterator<'_, T> {
     type Item = T;
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -87,11 +158,27 @@ mod tests {
         assert_eq!(rest, vec![2, 3, 4, 5]);
 
         let tuples = &[("a".to_string(), 1), ("b".to_string(), 2)];
-        let mut tuples_iter = SliceIterator::new(tuples);
+        let tuples_iter = SliceIterator::new(tuples);
 
         let map: HashMap<_, _> = tuples_iter.collect();
         assert_eq!(map.len(), 2);
         assert_eq!(map.get("a"), Some(&1));
         assert_eq!(map.get("b"), Some(&2));
+    }
+
+    #[test]
+    fn map_works() {
+        let slice_iter = SliceIterator::new(&[1, 2, 3, 4, 5]);
+
+        let map_list: Vec<_> = slice_iter.map(|x| x - 1).collect();
+        assert_eq!(map_list, vec![0, 1, 2, 3, 4]);
+    }
+
+    #[test]
+    fn filter_works() {
+        let slice_iter = SliceIterator::new(&[1, 2, 3, 4, 5]);
+
+        let filter_list: Vec<_> = slice_iter.filter(|x| *x > 3).collect();
+        assert_eq!(filter_list, vec![4, 5]);
     }
 }
