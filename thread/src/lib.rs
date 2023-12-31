f94df2a7cecc7c49d4cc881d64403b056ac37cb2
caelansar <819711623@qq.com>
2023-12-31 19:32:07 +0800

    feat(thread): test barrier

--- thread/src/lib.rs
+++ thread/src/lib.rs
@@ -242,4 +242,28 @@ mod tests {
         h4.join().unwrap();
         h5.join().unwrap();
     }
+
+    #[test]
+    fn test_barrier() {
+        use std::sync::{Arc, Barrier};
+        use std::thread;
+
+        let n = 10;
+        let mut handles = Vec::with_capacity(n);
+        let barrier = Arc::new(Barrier::new(n));
+        for _ in 0..n {
+            let c = Arc::clone(&barrier);
+            // The same messages will be printed together.
+            // You will NOT see any interleaving.
+            handles.push(thread::spawn(move || {
+                println!("before wait");
+                c.wait();
+                println!("after wait");
+            }));
+        }
+        // Wait for other threads to finish.
+        for handle in handles {
+            handle.join().unwrap();
+        }
+    }
 }
